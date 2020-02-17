96b673745c3e9575cbfc4488c10ec6bb51671464
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-02-17 11:09:37 -0500

    Added timing debugging

--- project1/Code/test.py
+++ project1/Code/test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import time
 import code
 import numpy as np
 import cv2
@@ -16,22 +17,33 @@ TEMPLATE_FILE = "../Data/reference_images/Lena.png"
 if __name__ == "__main__":
     
     # set debugging on (this will stop at every frame!)
-    ARDetector.debug(False)
+    ARDetector.debug(True)
     
     # get template file
     template = file_utils.imread(TEMPLATE_FILE)
     template_corners = get_corners(template)
 
     for ret,frame in file_utils.VidGenerator(TEST_FILE):
+        st = time.time()
         detector = ARDetector(frame)
         detections = detector.detect()
+        print("{:3f}s to detect.".format(time.time()-st))
 
         for corners, orientation in detections:
             # get the mapping from template pixels to image pixels
-            H = get_homography(corners, template_corners, 0)
-            warped = warp_image(template, H, frame.shape)
 
+            st = time.time()
+            H = get_homography(corners, template_corners, 0)
+            print("{:3f}s to get homography.".format(time.time()-st))
+            
+            st = time.time()
+            warped = warp_image(template, H, frame.shape)
+            print("{:3f}s to warp.".format(time.time()-st))
+
+            st = time.time()
             test = replace_section(frame, warped, corners)
+            print("{:3f}s to replace.".format(time.time()-st))
+            
             ARDetector.plot(test)
             # ARDetector.plot(frame)
             # ARDetector.plot(warped)
