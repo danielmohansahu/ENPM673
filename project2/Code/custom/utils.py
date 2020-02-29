fece7783b24ac6f3a7db0db12fd69801e2e0fd5f
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-02-29 10:59:05 -0500

    Handy debugging plotting function

--- project2/Code/custom/utils.py
+++ project2/Code/custom/utils.py
@@ -1,6 +1,10 @@
 """Collection of miscellaneous useful functions.
 """
+import cv2
 import time
+
+# global variables
+STOP_PLOTTING=False
 
 class Timer(object):
     def __init__(self, description, verbosity=0):
@@ -13,3 +17,21 @@ class Timer(object):
         if self.verbosity > 1:
             self.end = time.time()
             print(f"{self.description}: {self.end - self.start}")
+    
+def plot(frame, text="frame"):
+    """Plot a given frame.
+    """
+    global STOP_PLOTTING
+    if STOP_PLOTTING:
+        return
+
+    # convenience function for debugging
+    cv2.namedWindow(text, cv2.WINDOW_NORMAL)
+    cv2.imshow(text, frame)
+
+    key = cv2.waitKey(0)
+    if key == ord('q'):
+        # stop debugging on 'q'
+        STOP_PLOTTING=True
+
+    cv2.destroyAllWindows()
