f2e192a6925d8c08dee452569232af553a90f597
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-03-29 09:03:21 -0400

    Initial commit of (empty) project3 code

---
+++ project3/Code/custom/maximize.py
@@ -0,0 +1,20 @@
+"""Implementation of the Expectation Maximization Algorithm for a Gaussian distribution."""
+
+import numpy as np
+
+class ExpectationMaximization:
+    def __init__(self):
+        ...
+
+    def solve(self):
+        ...
+
+    def _initialize(self):
+        ...
+
+    def _expectation(self):
+        ...
+
+    def _maximization(self):
+        ...
+        
---
+++ project3/Code/test.py
@@ -0,0 +1,6 @@
+
+from custom.maximize import ExpectationMaximization as EM
+
+if __name__=="__main__":
+    em = EM()
+
