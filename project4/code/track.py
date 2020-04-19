944486903edfa19e7026e7574eced9a094fb2f46
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-04-19 13:25:13 -0400

    Miscellaneous cleanup; added command line arguments to switch datasets; less spam

--- project4/code/track.py
+++ project4/code/track.py
@@ -1,24 +1,37 @@
 #!/usr/bin/env python3
 
 import os
+import sys
+import time
 import glob
+import argparse
 import cv2
 import numpy as np
-from scipy import ndimage
 from custom.LucasKanade import LucasKanade
 from custom import file_utils
 
-# TEMPLATE_BBOX=[269,75,34,64]
-# FILEPATH="../data/Bolt2/img/"
-# VIDEOFILE="Bolt2.mp4"
+DATASETS = {
+    'bolt': {
+        'bbox': [269,75,34,64],
+        'filepath': "../data/Bolt2/img/",
+        'videofile': "Bolt2.mp4"},
+    'car': {
+        'bbox': [70,51,107,87],
+        'filepath': "../data/Car4/img/",
+        'videofile': "Car4.mp4"},
+    'dragonbaby': {
+        'bbox': [160,83,56,65],
+        'filepath': "../data/DragonBaby/img/",
+        'videofile': "DragonBaby.mp4"}
+}
 
-TEMPLATE_BBOX=[70,51,107,87]
-FILEPATH="../data/Car4/img/"
-VIDEOFILE="Car4.mp4"
-
-# TEMPLATE_BBOX=[160,83,56,65]
-# FILEPATH="../data/DragonBaby/img/"
-# VIDEOFILE="DragonBaby.mp4"
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--dataset", required=True, help="Dataset to run (choose from {}".format(DATASETS.keys()))
+    args = parser.parse_args()
+    if args.dataset not in DATASETS.keys():
+        raise RuntimeError("Invalid dataset. Got {}, expected {}".format(args.dataset, DATASETS.keys()))
+    return args
 
 def draw_rectangle(frame, bb, affine):
     """Perform the given affine transformation on the bounding box and draw it on the frame.
@@ -39,27 +52,34 @@ def draw_rectangle(frame, bb, affine):
     for pt in pts:
         rot = np.dot(M,pt)[:2]
         rot_pts.append(rot)
+
+    # draw min bounding rectangle
     ctr = np.array(rot_pts, dtype = np.float32)
-    width = bb[2]
-    height = bb[3]
     x,y,w,h = cv2.boundingRect(ctr)
-    result = cv2.rectangle(frame,(x,y),(x+width,y+height),(0,255,0),2)
-    # draw rectangle
+    result = cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+
+    # draw affine rectangle (for reference)
     result = cv2.polylines(frame, np.array([rot_pts],np.int32), True, (0,0,255))
 
     return result
 
 if __name__ == "__main__":
+    # parse args
+    args = parse_args()
+    filepath = DATASETS[args.dataset]['filepath']
+    bbox = DATASETS[args.dataset]['bbox']
+    videofile = DATASETS[args.dataset]['videofile']
+
     # get all image files (in order)
-    images = glob.glob(FILEPATH+"*.jpg")
+    images = glob.glob(filepath+"*.jpg")
     images.sort()
 
     # initialize tracker with initial frame and bounding box
     template = cv2.imread(images[0],0)
-    lk = LucasKanade(template,TEMPLATE_BBOX)
+    lk = LucasKanade(template,bbox)
 
     # generate video reader / writer objects
-    output_file = "processed_" + os.path.basename(VIDEOFILE)
+    output_file = "processed_" + os.path.basename(videofile)
     video_writer = file_utils.VidWriter(output_file, cv2.VideoWriter_fourcc(*'mp4v'), 5, (template.shape[1],template.shape[0]), isColor=False)
 
     # step through each frame and process
@@ -68,22 +88,37 @@ if __name__ == "__main__":
         # draw first image
         frame = cv2.imread(images[0],0)
         images.pop(0)
-        result = draw_rectangle(frame, TEMPLATE_BBOX, p)
-
+        result = draw_rectangle(frame, bbox, p)
         writer.write(result)
 
+        # initialize metavariables
+        count = 1
+        failures = 0
+        st = time.time()
+
         for image in images:
-            print("Processing frame: {}".format(image))
+            # update status bar
+            count += 1
+            bar = ("="*int(20*count/len(images))).ljust(20)
+            sys.stdout.write('\r')
+            sys.stdout.write("[{}] {} {:.0f}s, {} bad".format(bar, "/".join(image.split("/")[-3:]), time.time()-st, str(failures)))
+            sys.stdout.flush()
+
+            # read frame
             frame = cv2.imread(image,0)
 
             # estimate tracked transform
-            p = lk.estimate(frame)
+            success, p = lk.estimate(frame)
 
             if p is not None:
                 # draw rectangle
-                result = draw_rectangle(frame, TEMPLATE_BBOX, p)
+                result = draw_rectangle(frame, bbox, p)
             else:
                 # just draw an empty image
                 result = frame.copy()
+
+            # update failure / non-converged count
+            if not success:
+                failures += 1
 
             writer.write(result)
